35aa1398b19d4a204cd060c8bee47413d64d3470
Gurveen <gurveens11@gmail.com>
2021-05-12 16:11:15 +0530

    Style theme toggle

--- src/components/ToggleTheme.js
+++ src/components/ToggleTheme.js
@@ -2,10 +2,11 @@ import React, { useContext } from 'react'
 import { ThemeContext } from '../contexts/ThemeContext'
 
 const ToggleTheme = () => {
-	const { toggleTheme } = useContext(ThemeContext)
+	const { isLightTheme, toggleTheme } = useContext(ThemeContext)
 	return (
 		<button className="toggle-btn" onClick={toggleTheme}>
-			Toggle
+			<i className="fas fa-moon" style={{ opacity: isLightTheme ? 1 : 0 }}></i>
+			<i className="fas fa-sun" style={{ opacity: isLightTheme ? 0 : 1 }}></i>
 		</button>
 	)
 }
